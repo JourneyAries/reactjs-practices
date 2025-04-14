308c8c364dc0c5dbb361f0a25a929f9c4898bd82
JourneyAries <journeyaries13@gmail.com>
2025-04-15 05:19:44 +0700

    [faq-accordion](v1): finish 100%

--- faq-accordion/src/App.jsx
+++ faq-accordion/src/App.jsx
@@ -1,10 +1,26 @@
+import { FiMinus, FiPlus } from 'react-icons/fi';
+import { items } from './data/data';
+
 function App() {
 	return (
-		<div>
-			<details>
-				<summary>What is Github and how does it work?</summary>
-				<p>Check our real-time status report</p>
-			</details>
+		<div className='max-w-[360px] flex flex-col gap-y-3 bg-white rounded px-2 py-4'>
+			{items.map((item, index) => {
+				return (
+					<details
+						className='group border-b-1 open:border-b-3 open:border-amber-600'
+						key={index}>
+						<summary className='text-lg text-slate-900 font-bold list-none flex justify-between items-start cursor-pointer py-1'>
+							<span className='group-open:italic'>{item.title}</span>
+							<span
+								className={`text-2xl transition-transform duration-300 group-open:rotate-180 `}>
+								<FiPlus className='group-open:hidden block' />
+								<FiMinus className='group-open:block hidden' />
+							</span>
+						</summary>
+						<p className='text-sm text-slate-900 pb-2'>{item.content}</p>
+					</details>
+				);
+			})}
 		</div>
 	);
 }
