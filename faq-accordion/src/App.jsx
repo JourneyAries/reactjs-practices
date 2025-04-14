d305f2936bc547b02d47b6e482ef67beab792e69
JourneyAries <journeyaries13@gmail.com>
2025-04-15 04:28:54 +0700

    [faq-accordion](v1): initialization

---
+++ faq-accordion/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+	return (
+		<div>
+			<details>
+				<summary>What is Github and how does it work?</summary>
+				<p>Check our real-time status report</p>
+			</details>
+		</div>
+	);
+}
+
+export default App;
